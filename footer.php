b08c21009dc8ac2fc31eb7d1ccbb108517c41f92
U-NORVAX\bpatrianakos <bpatrianakos@bpatrianakos.norvax.net>
2013-02-14 17:32:51 -0600

    Getting the design started. Header HTML is done and Homepage HTL is also about done

--- footer.php
+++ footer.php
@@ -18,11 +18,17 @@
 	<script>window.jQuery || document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');</script>
 	
 	<!-- Google Analytics -->
-	<script>
-		var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
-		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		g.src='//www.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g,s)})(document,'script');
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-38493313-1']);
+	  _gaq.push(['_setDomainName', 'userbillofrights.org']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
 	</script>
 	
 	<?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -1,74 +1,44 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	
-	<!-- DNS Prefetch -->
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<link rel="dns-prefetch" href="//www.google-analytics.com">
-	
+
 	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-	
-	<!-- Meta -->
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<meta name="description" content="<?php bloginfo('description'); ?>">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		
-	<!-- CSS + jQuery + JavaScript -->
+	<meta name="description" content="<?php bloginfo('description'); ?>" />
+	<meta name="author" content="Bill Patrianakos" />
+	<link rel="author" href="<?php bloginfo('template_url'); ?>/humans.txt" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css?v=2" />
+	<script src="<?php bloginfo('template_url'); ?>/js/libs/respond.min.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--[if gte IE 9]>
+	<style type="text/css">
+	.gradient { filter: none; }
+	</style>
+	<![endif]-->
 	<?php wp_head(); ?>
-	<script>
-	conditionizr({
-		debug      : false,
-		scriptSrc  : 'js/conditionizr/',
-		styleSrc   : 'css/conditionizr/',
-		ieLessThan : { active: true, version: '9', scripts: true, styles: true, classes: true, customScript: '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js'},
-		chrome     : { scripts: true, styles: true, classes: true, customScript: false },
-		safari     : { scripts: true, styles: true, classes: true, customScript: false },
-		opera      : { scripts: true, styles: true, classes: true, customScript: false },
-		firefox    : { scripts: true, styles: true, classes: true, customScript: false },
-		ie10       : { scripts: true, styles: true, classes: true, customScript: false },
-		ie9        : { scripts: true, styles: true, classes: true, customScript: false },
-		ie8        : { scripts: true, styles: true, classes: true, customScript: false },
-		ie7        : { scripts: true, styles: true, classes: true, customScript: false },
-		ie6        : { scripts: true, styles: true, classes: true, customScript: false },
-		retina     : { scripts: true, styles: true, classes: true, customScript: false },
-		mac    : true,
-		win    : true,
-		x11    : true,
-		linux  : true
-	});
-	</script>
-	
 </head>
+
 <body <?php body_class(); ?>>
 
-	<!-- Header -->
-	<header>
-	
-		<!-- Wrapper -->
-		<div class="wrapper">
-		
-			<!-- Logo -->
-			<div id="logo">
-				<a href="<?php echo home_url(); ?>">
-					<!-- SVG Logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-					<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
-				</a>
-			</div>
-			<!-- /Logo -->
-			
-			<!-- Nav -->
-			<nav>
+	<header class="container">
+		<section class="row">
+			<article class="five">
+				<h1 class="logo">UBR <span class="subtitle">The Internet User's Bill of Rights</span></h1>
+			</article>
+			<nav class="seven">
 				<?php html5blank_nav(); ?>
 			</nav>
-			<!-- /Nav -->
-			
-			<br class="clear">
-			
-		</div>
-		<!-- /Wrapper -->
-	
+		</section>
 	</header>
-	<!-- /Header -->
-	
-	<!-- Wrapper -->
-	<div class="wrapper">
