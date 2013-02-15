aa3585d0fca30c33ab2aa63fb129e27f88f4bbab
Bill Patrianakos <bill@billpatrianakos.me>
2013-02-15 13:05:06 -0600

    Fixed submit form checkboxes (Applies only to forms created with contact-form-7 plugin)

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 This is the Wordpress theme for the [Internet User's Bill of Rights](http://userbillofrights.org) website. The User Bill of Rights came about from [this HN discussion](http://news.ycombinator.com/item?id=5220072). The site should be up within a few hours.
 
 You're free to use this design but the content cannot be used verbatim.
+
+## The Original User's Bill of Rights
+
+1. Notice of changes to service and policies
+
+If a policy is going to change or a feature is going to be added, removed, or modified we should get some sort of notice via email, blog post or preferably both.
+
+2. The right of free users to keep their active accounts for as long as the service stays in business
+If I have a free Google account I expect that so long as I'm using it reasonably often Google won't decide one day to just get rid of all free accounts.
+
+3. The right to continue using the core offering regardless of feature additions, subtractions, or modifications for any other account type.
+So as an Instagram user I should be able to take, post, like, and comment on photos as that is the very core of the product. If one day Instagram has paid accounts I'm not entitled to any of the upgraded features but I should never lose the ability to use the service in its most basic ways because I'm using it free.
+
+4. For services to act in accordance with applicable laws and, more importantly, strive to never enter legal "gray area" in matters that directly affect users.
--- page-home.php
+++ page-home.php
@@ -18,7 +18,7 @@
 	<section id="ticker" class="container">
 		<section class="row">
 			<article class="twelve centered-text">
-				<p>Do you run a web service that offers free accounts? <a href="<?php site_url('/') ?>add-service">Get Listed!</a></p>
+				<p>Do you run a web service that offers free accounts? <a href="<?php echo home_url('/add-service') ?>">Get Listed!</a></p>
 			</article>
 		</section>
 	</section>
@@ -30,7 +30,7 @@
 					Whether you’re paying for a service or not, you have rights - just not as many as you probably think. User entitlement, especially in the tech scene, is at epidemic proportions. In an effort to help curb this behavior we’ve set up a list of rights we’d like the web community to embrace. If both users and service providers accept these terms users can then know who they can trust and what they can reasonably expect from a service before signing up. Check out our list of supporting services and read up on your rights.
 				</p>
 				<p>
-					<a class="bucket-link" href="<?php site_url('/'); ?>rights">Read your rights</a> or <a class="bucket-link" href="<?php site_url('/'); ?>services/all-services">See supporting services</a>
+					<a class="bucket-link" href="<?php echo home_url('/rights'); ?>">Read your rights</a> or <a class="bucket-link" href="<?php echo home_url('/services/all-services'); ?>">See supporting services</a>
 				</p>
 			</article>
 			<article class="five">
@@ -39,7 +39,7 @@
 					You’r not off the hook here! With so many new web sites, apps, and other services popping up every day users don’t know who they can trust to stick around, handle their data responsibly, and act in good faith when it comes time to monetize. By adding your service to our list you’re showing your committment to the user’s rights even - and especially - when they’re using your service for free. Don’t worry, we’re actually against users feeling so entitled lately too and we think you’ll find the User’s Bill of Rights quite reasonable.
 				</p>
 				<p>
-					<a class="bucket-link" href="<?php site_url('/'); ?>rights">Read the rights</a> or <a class="bucket-link" href="<?php site_url('/'); ?>add-service">Be added as a supporter</a>
+					<a class="bucket-link" href="<?php echo home_url('/rights'); ?>">Read the rights</a> or <a class="bucket-link" href="<?php echo home_url('/add-service'); ?>">Be added as a supporter</a>
 				</p>
 			</article>
 			<aside class="two last">
